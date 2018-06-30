1540eca59d63101b795d69cba2532474b995273d
Ram <Ram@RAM-PC>
2018-06-30 06:15:30 +0530

    Routing part2 practicing

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -29,10 +29,6 @@ class Main extends Component {
 
     }
 
-    onDishSelect(dishId){
-        this.setState({ selectedDish : dishId });
-    }
-
     render() {
 
         const HomePage = () => {
@@ -45,18 +41,27 @@ class Main extends Component {
             );
         }
 
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetails
+                    dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))[0]}
+                />
+            )
+        }
+
         return (
             <div>
                 <Header/>
                 <Switch>
                     <Route path="/home" component={HomePage}/>
-                    <Route exact path="/menu" component={ () =>
-                        (<Menu dishes={this.state.dishes} onClick={(dishId) => (this.onDishSelect(dishId))} />)} />
+                    <Route exact path="/menu" component={ () => (<Menu dishes={this.state.dishes} />)} />
+                    <Route path="/menu/:dishId" component={ DishWithId } />
                     <Route exact path="/contactus" component={Contact} />
-                    <Redirect to="/home"/>
+                    <Redirect to="/home" />
                 </Switch>
                 {/*<Menu dishes={this.state.dishes} onClick={(dishId) => (this.onDishSelect(dishId))}/>*/}
-                <DishDetails dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+                {/*<DishDetails dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>*/}
                 <Footer/>
             </div>
         );
